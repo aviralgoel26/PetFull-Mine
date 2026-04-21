b77cff12779dc1a35358150e96e6ccd046bdfc1e
Aviral Goel <aviralgoel9th@gmail.com>
2026-04-21 23:59:32 +0530

    Fixing Cors Config

--- PetfullBackend/backend/src/main/java/com/petfull/backend/config/CorsConfig.java
+++ PetfullBackend/backend/src/main/java/com/petfull/backend/config/CorsConfig.java
@@ -1,5 +1,25 @@
 package com.petfull.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
-    
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://petfull.netlify.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+.allowCredentials(true);
+            }
+        };
+    }
 }
